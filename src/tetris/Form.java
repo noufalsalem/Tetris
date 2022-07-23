a160f2de172a41776595f47e8413aa21c7b0eaaf
Nouf AlSalem <61601877+noufalsalem@users.noreply.github.com>
2022-07-23 08:22:06 +0300

    Add files via upload

---
+++ src/tetris/Form.java
@@ -0,0 +1,71 @@
+package tetris;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Form {
+	Rectangle a;
+	Rectangle b;
+	Rectangle c;
+	Rectangle d;
+	Color color;
+	private String name;
+	public int form = 1;
+
+	public Form(Rectangle a, Rectangle b, Rectangle c, Rectangle d) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+	}
+
+	public Form(Rectangle a, Rectangle b, Rectangle c, Rectangle d, String name) {
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		this.d = d;
+		this.name = name;
+
+		switch (name) {
+		case "j":
+			color = Color.DARKGRAY;
+			break;
+		case "l":
+			color = Color.RED;
+			break;
+		case "o":
+			color = Color.TOMATO;
+			break;
+		case "s":
+			color = Color.GREEN;
+			break;
+		case "t":
+			color = Color.ORANGE;
+			break;
+		case "z":
+			color = Color.BLUE;
+			break;
+		case "i":
+			color = Color.GOLD;
+			break;
+
+		}
+		this.a.setFill(color);
+		this.b.setFill(color);
+		this.c.setFill(color);
+		this.d.setFill(color);
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void changeForm() {
+		if (form != 4) {
+			form++;
+		} else {
+			form = 1;
+		}
+	}
+}
